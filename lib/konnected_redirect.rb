234dcde2bdc313f2c4bafa3c7512f2827dd9a058
Nate Clark <nate@nateclark.com>
2018-05-14 12:02:23 -0400

    redirect smartapp installation pages

--- lib/konnected_redirect.rb
+++ lib/konnected_redirect.rb
@@ -6,7 +6,16 @@ module Rack
         "https://help.konnected.io/support/solutions/articles/32000023470-flashing-new-konnected-firmware-software",
 
       '/security-alarm-system/troubleshooting/discovery-cant-find-device' =>
-        "https://help.konnected.io/support/solutions/articles/32000023644-device-discovery-troubleshooting"
+        "https://help.konnected.io/support/solutions/articles/32000023644-device-discovery-troubleshooting",
+
+      '/security-alarm-system/installation/connect-wifi' =>
+        "https://help.konnected.io/support/solutions/articles/32000022408-connect-to-wifi",
+
+      '/security-alarm-system/installation/github-integration' =>
+        "https://help.konnected.io/support/solutions/articles/32000022409-smartthings-enable-github-integration",
+
+      '/security-alarm-system/installation/smartapp' =>
+        "https://help.konnected.io/support/solutions/articles/32000022410-smartthings-install-the-konnected-smartapps-with-github"
     }
 
     def initialize(app)
